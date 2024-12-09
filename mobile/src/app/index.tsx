751b62ca623b0791b13d2777b6168f7d451fd34d
felipe <felipenarciso15@gmail.com>
2024-12-09 20:52:03 -0300

    feat: commit inicial

---
+++ mobile/src/app/index.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from  "react-native";
+
+const Index = () => {
+    return (
+        <View>
+            <Text>Hello react native</Text>
+        </View>
+    )
+}
+
+export default Index;
